8722a6b6476478251215ff32bd2de72984f4fd4f
lswteen <lswteen@gmail.com>
2025-12-04 18:40:41 +0900

    add : boilerplate-manve-21

---
+++ src/main/java/com/lottecard/mobileid/mobileid/MobileidApplication.java
@@ -0,0 +1,13 @@
+package com.lottecard.mobileid.mobileid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lottecard/mobileid/mobileid/MobileidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lottecard.mobileid.mobileid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
